cbbc706fc5d9b5043c1702e62d863c215d19dcf9
muhammed Febin Rafnad <muhammedfebinrafnadth@gmail.com>
2024-12-12 22:20:02 +0530

    updated

---
+++ class.dart
@@ -0,0 +1,17 @@
+class Boy{
+  String? _name;
+  int? _age;
+ String getname(){
+  return this._name!;
+
+ }
+ int getage(){
+  return this._age!;
+ }
+ void setname(String name){
+ this._name=name;
+ }
+ void setage(int age){
+  this._age=age;
+ }
+}
---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'class.dart';
+
+void main(){
+  Boy d=Boy();
+  d.setname("jack");
+  d.setage(7);
+  print(d.getname());
+  print(d.getage());
+}
